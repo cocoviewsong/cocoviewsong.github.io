e7f2c7711659a6fb1dcdbc9b3cdb43822ba4de1b
cocoviewsong <2951171089@qq.com>
2024-11-16 21:15:10 +0800

    node deploy.js

--- assets/use-expose-Dmhk7UWQ.js
+++
@@ -1,1 +0,0 @@
-import{k as t,g as s}from"./index-D6uaYdbn.js";function r(n){const e=s();e&&t(e.proxy,n)}export{r as u};
---
+++ assets/use-expose-KrhNu2GM.js
@@ -0,0 +1,1 @@
+import{q as t,i as s}from"./index-DmBhjwxV.js";function r(n){const e=s();e&&t(e.proxy,n)}export{r as u};
